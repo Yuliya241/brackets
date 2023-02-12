7092f785d0a61328d091733cf352c4d9843099a1
Yuliya Shelenhouskaya <yulchik241@mail.ru>
2023-02-13 00:08:23 +0300

    feat: write function to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let opened = bracketsConfig[j][0];
+      let closed = bracketsConfig[j][1];
+      let current = str[i];
+      let last = stack[stack.length - 1];
+      if (last == opened && current == closed) {
+        stack.pop();
+      } else if (current == opened) {
+          stack.push(current);
+        } else if (stack.length == 0 && current == closed) {
+          return false;
+        }  
+      } 
+    }
+  return stack.length === 0
+} 
+
